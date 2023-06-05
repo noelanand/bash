ba1b59846123ce7d68d2f6ffd0f7bb4ec159a9c3
noel anand <chakramanand@gmail.com>
2023-06-05 10:42:39 +0530

    commiting my first file

---
+++ for.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "enter the how many number to print"
+read i
+for ((j=0; j<=$i; j++ ))
+do 
+if (( $j % 2 == 0 ))
+then
+echo "$j this is evn number"
+else
+echo "$j this is odd number"
+fi
+done
